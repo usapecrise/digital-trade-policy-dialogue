fba3af5bd9a06977ab4055755ecabcf9227cb3f8
usapecrise <usapecrise@gmail.com>
2026-04-15 16:28:55 -0400

    Update agency.js

--- css/2016_style/js/agency.js
+++ css/2016_style/js/agency.js
@@ -8,9 +8,13 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+
+        var navHeight = $('.navbar-default').outerHeight();
+
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - navHeight - 60
+        }, 800, 'easeInOutExpo');
+
         event.preventDefault();
     });
 });
